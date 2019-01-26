c2e61d70a9f5addcc0a8b703b7f9596ec94f8f60
guitorioadar <guitorioadar@gmail.com>
2019-01-26 10:49:08 +0600

    main app comment

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation'
 import MainScreen from "./Components/MainScreen";
 
+// this is the main app
+
 export default class App extends React.Component{
     render(){
         return(
